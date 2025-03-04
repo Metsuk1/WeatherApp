c0bcc3710665640291f639877c4502770ccaa592
Abayev <240918@astanait.edu.kz>
2025-03-04 14:26:07 +0500

    ...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import services.weatherGui;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-
+                new weatherGui().setVisible(true);
             }
         });
 
---
+++ src/main/java/services/weatherGui.java
@@ -0,0 +1,15 @@
+package services;
+
+import javax.swing.*;
+
+public class weatherGui extends JFrame {
+    public weatherGui() {
+        super("Weather");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+
+    }
+
+}
